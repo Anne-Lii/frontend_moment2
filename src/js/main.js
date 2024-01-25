ea9df5069af61296d8108e80081e45460b341bd9
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-01-25 15:55:46 +0100

    fått till sortering att toggla vid varje klick på rubrik

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,71 @@
+/* kod skriven av Anne-Lii Hansen anha2324@student.miun.se*/
+
 "use strict"
 
-const url = "https://dahlgren.miun.se/ramschema_ht23.php";
+window.onload = init;
 
-async function getData() {
+let toggleSort = 1;
 
-   try {
-        const respons = await fetch(url);
-        const data = await respons.json();
-        console.table(data);
+//fetch-anrop och konvertera till js
+async function init() {
+    const url = "https://dahlgren.miun.se/ramschema_ht23.php"; //API adressen
 
-        const courses = [data];      
-
-        data.forEach((courses)=>{
-            document.getElementById("kurser").innerHTML += "<li>" + courses.code + "</li>"
-        });
-
-        data.forEach((courses)=>{
-            document.getElementById("namn").innerHTML += "<li>" + courses.coursename + "</li>"
-        });
-
-        data.forEach((courses)=>{
-            document.getElementById("progression").innerHTML += "<li>" + courses.progression + "</li>"
-        });
-
+    try{        
+        const response = await fetch(url); 
+        let data = await response.json();
         
-
+        const headlines = document.getElementsByTagName("th");
+        for (let i=0; i<headlines.length;i++) {
+        headlines[i].addEventListener("click", function() {
+            sortData(data, i);
+        });  
+        displayData(data);   
+    }
     } catch (error) {
-        document.getElementById("kurser").innerHTML = "Det gick inte att hämta kurser";
+        console.log("nu blev de knasigt", error);
     }
 }
 
-getData();
+function sortData (data, headline){
+    //sparar datan så den inte ändras
+    const sortedData = [...data]; 
+
+    toggleSort *= -1;
+
+    switch (headline) {
+        case 0: //kurskoden index 0
+            sortedData.sort((a, b) => toggleSort * a.code.localeCompare(b.code));
+            break;
+
+        case 1://kursnamnet index 1
+            sortedData.sort((a, b) => toggleSort * a.coursename.localeCompare(b.coursename));
+            break;
+
+        case 2://progressionen index 2
+            sortedData.sort((a, b) => toggleSort * a.progression.localeCompare(b.progression));
+            break;
+
+        default:
+            break;
+    } 
+
+    displayData(sortedData);  //skickar uppdaterade datan till displaydata funktionen
+   
+}
+
+function displayData(data) {
+    const listEl = document.getElementById("list");   
+    listEl.innerHTML = ""; //rensar tidigare lista 
+
+    //Loopa igenom och skriv ut till sidan
+    data.forEach((courses) => {
+
+        listEl.innerHTML += `
+        <tr>
+            <td>${courses.code}</td>
+            <td>${courses.coursename}</td>
+            <td>${courses.progression}</td>
+        </tr>
+        `;               
+    });
+}
