0c1cb129a2f5be951626d441f1e87aa6bb6d8fdd
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-01-26 09:11:54 +0100

    ordnat och stylat inputfältet och ordnat filtrering från input

--- src/js/main.js
+++ src/js/main.js
@@ -4,33 +4,54 @@
 
 window.onload = init;
 
-let toggleSort = 1;
+let toggleSort = 1;//global variabel som lagrar värdet 1 för toggling.
 
 //fetch-anrop och konvertera till js
 async function init() {
+
     const url = "https://dahlgren.miun.se/ramschema_ht23.php"; //API adressen
 
-    try{        
-        const response = await fetch(url); 
-        let data = await response.json();
-        
+    try {
+        const response = await fetch(url);//inväntas svar från fetchen
+        let data = await response.json();//inväntar svar från konverteringen till js
+
         const headlines = document.getElementsByTagName("th");
-        for (let i=0; i<headlines.length;i++) {
-        headlines[i].addEventListener("click", function() {
-            sortData(data, i);
-        });  
-        displayData(data);   
-    }
+        for (let i = 0; i < headlines.length; i++) {
+            headlines[i].addEventListener("click", function () {
+                sortData(data, i);
+            });
+
+            //skickar data från API vidare till andra funktioner
+            filterInput(data);
+            displayData(data);
+        }
     } catch (error) {
-        console.log("nu blev de knasigt", error);
+        
     }
 }
 
-function sortData (data, headline){
-    //sparar datan så den inte ändras
-    const sortedData = [...data]; 
+//filtrering från sökrutan
+function filterInput(data) {
+    let inputEl = document.getElementById("sok");
+    inputEl.addEventListener("keyup", function () {
+        let filter = inputEl.value.toUpperCase();
+    
+        let filteredData = data.filter(course => {
+            return course.code.toUpperCase().includes(filter) ||
+                course.coursename.toUpperCase().includes(filter) ||
+                course.progression.toUpperCase().includes(filter);
+    
+        });    
 
-    toggleSort *= -1;
+        displayData(filteredData)    
+    });
+}
+
+function sortData(data, headline) {
+    //sparar datan så den inte ändras
+    const sortedData = [...data];
+
+    toggleSort *= -1;//multiplicerar variabeln med -1 för att toggla mellan 1 (true) och -1 (false).
 
     switch (headline) {
         case 0: //kurskoden index 0
@@ -47,14 +68,13 @@ function sortData (data, headline){
 
         default:
             break;
-    } 
+    }
 
     displayData(sortedData);  //skickar uppdaterade datan till displaydata funktionen
-   
 }
 
 function displayData(data) {
-    const listEl = document.getElementById("list");   
+    const listEl = document.getElementById("list");
     listEl.innerHTML = ""; //rensar tidigare lista 
 
     //Loopa igenom och skriv ut till sidan
@@ -66,6 +86,6 @@ function displayData(data) {
             <td>${courses.coursename}</td>
             <td>${courses.progression}</td>
         </tr>
-        `;               
+        `;
     });
 }
